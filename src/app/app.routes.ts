92a6f607429dbe407d470b5b7e2ff5bf260dd3d7
anyiramirez <anyiramirez@unicauca.edu.co>
2024-04-05 00:30:07 -0500

    Redireccionamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,6 @@ import { HomeComponent } from "./home/home.component";
 
 
 export const routes: Routes = [
-    {
-        path: '', redirectTo:'home' , pathMatch:'full'
-    },
     
     {
         path:'home',
